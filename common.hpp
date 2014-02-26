8f3cfd6404a41f3f34395d266ec7c07304f2e2b5
Chocobo1 <Chocobo1@users.noreply.github.com>
2014-02-26 22:18:25 +0800

    forgot to include in 36a5771e687492f663df56bda8bd0f31c158a5ab

---
+++ common.hpp
@@ -0,0 +1,10 @@
+#ifndef common_HPP
+#define common_HPP
+
+#include <cstdint>
+
+
+bool diffOneBit( const uint32_t a, const uint32_t b );
+bool checkMasked( const uint32_t a , const uint32_t a_mask , const uint32_t b );
+
+#endif
