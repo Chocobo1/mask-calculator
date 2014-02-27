0b10c5b2f407e8fb705de40ce695b9146d9ceda8
Chocobo1 <Chocobo1@users.noreply.github.com>
2014-02-27 20:00:08 +0800

    update progress

--- qmm.cpp
+++ qmm.cpp
@@ -1,8 +1,13 @@
 #include "qmm.hpp"
 
+#include "common.hpp"
 #include <forward_list>
 
-#include "common.hpp"
+
+size_t QMM::MyMmapHash::operator()( const MyMmap::const_iterator &a ) const
+{
+	return std::hash< const MyMmap::value_type * >()( &( *a ) );
+}
 
 
 void QMM::addNum( const uint32_t a )
@@ -29,18 +34,65 @@ void QMM::addNum( const uint32_t a , const uint32_t b )
 
 void QMM::doCalc()
 {
+	// insert & combine & merge values
+	std::unordered_set< MyMmap::const_iterator , MyMmapHash > rm_list;
+	for( const auto &i : my_input )
+	{
+		stage2( i , UINT_MAX , rm_list );
+//		printf("\n");
+	}
+	printf( "my_multimap.size(): %zu\n" , my_multimap.size() );
+	printf( "rm_list.size(): %zu\n" , rm_list.size() );
+
+	fprintf( stderr, "kero1\n" );
+
+	// filter out used values
+//	size_t c = 1;
+	for( const auto &i : rm_list )
+	{
+//		printf( "%zu: %u/0x%x\n" , c++ , i->second , i->first );
+//		my_multimap.erase( i );
+	}
+	//rm_list.clear();
+
+	fprintf( stderr, "kero2\n" );
+
+	// remove redundancy
+	removeRedundancy();
+
 	return;
 }
 
 
-void QMM::calcMask( const uint32_t my_val , const uint32_t my_mask )
+void QMM::stage2( const uint32_t my_val , const uint32_t my_mask , std::unordered_set<MyMmap::const_iterator , MyMmapHash > &rm_list )
 {
-	return;
-}
+	const auto my_val_itr = my_multimap.emplace( my_mask , my_val );
 
+//	printf( "\n [s2] start: %u/0x%x\n" , my_val , my_mask );
+	const auto t = my_multimap.equal_range( my_mask );
+	for( auto j = t.first ; j != t.second ; ++j )
+	{
+		// try to merge with other values
+		const auto other_val = j->second;
+		const bool if_merge = diffOneBit( my_val , other_val );
+//		printf( " [s2] other_val: %u/0x%x, result: %d\n" , other_val , my_mask , if_merge );
+		if( if_merge )
+		{
+			const uint32_t new_mask = my_mask & ( ~( my_val xor other_val ) );
+			const uint32_t new_val = std::min( my_val , other_val );
 
-void QMM::stage2( const uint32_t my_val , const uint32_t my_mask )
-{
+			// record used values
+			rm_list.emplace( j );
+			rm_list.emplace( my_val_itr );
+//			printf( " [s2] rm_list.emplace, %u/0x%x\n" , j->second , my_mask );
+//			printf( " [s2] rm_list.emplace, %u/0x%x\n" , my_val_itr->second , my_mask );
+
+//			printf( " [s2] recursive\n" );
+			stage2( new_val , new_mask , rm_list );
+		}
+	}
+
+//	printf( " [s2] end\n" );
 	return;
 }
 
@@ -84,11 +136,11 @@ void QMM::applyPetrickMethod()
 	// multiply
 	// stuff in first
 	std::forward_list< std::unordered_set< size_t > > aaa;
-	for( auto &i : my_input )
+	for( const auto &i : my_input )
 	{
 		size_t map_index = 0;
 		std::unordered_set< size_t > tmp_set;
-		for( auto &j : tmp_map )
+		for( const auto &j : tmp_map )
 		{
 			if( checkMasked( j.second , j.first , i ) )
 			{
@@ -115,7 +167,32 @@ void QMM::applyPetrickMethod()
 }
 
 
-const std::multimap< uint32_t , uint32_t > *QMM::getOutput() const
+void QMM::removeRedundancy()
+{
+	for( auto i = my_multimap.begin() ; i != my_multimap.end() ; )
+	{
+		bool flag = false;
+		auto j = i; ++j;
+		for( ; j != my_multimap.end() ; ++j )
+		{
+			if( ( i->first == j->first ) && ( i->second == j->second ) )
+			{
+				flag = true;
+				break;
+			}
+		}
+
+		if( flag )
+			my_multimap.erase( i++ );
+		else
+			++i;
+	}
+
+	return;
+}
+
+
+const MyMmap *QMM::getOutput() const
 {
 	return &my_multimap;
 }
@@ -124,5 +201,6 @@ const std::multimap< uint32_t , uint32_t > *QMM::getOutput() const
 void QMM::reset()
 {
 	my_multimap.clear();
+	my_input.clear();
 	return;
 }
--- qmm.hpp
+++ qmm.hpp
@@ -1,9 +1,12 @@
 #ifndef qmm_HPP
 #define qmm_HPP
 
+#include <cstdint>
 #include <map>
 #include <unordered_set>
-#include <cstdint>
+
+
+typedef std::multimap< uint32_t , uint32_t > MyMmap;
 
 
 class QMM
@@ -13,16 +16,21 @@ class QMM
 		void addNum( const uint32_t a , const uint32_t b );
 
 		void doCalc();
-		const std::multimap< uint32_t , uint32_t > *getOutput() const;
+		const MyMmap *getOutput() const;
 
 		void reset();
 
 	private:
-		void calcMask( const uint32_t my_val , const uint32_t my_mask );
-		void stage2( const uint32_t my_val , const uint32_t my_mask );
+		struct MyMmapHash
+		{
+			size_t operator()( const MyMmap::const_iterator &a ) const;
+		};
+
+		void removeRedundancy();
+		void stage2( const uint32_t my_val , const uint32_t my_mask , std::unordered_set<MyMmap::const_iterator , MyMmapHash> &rm_list );
 		void applyPetrickMethod();
 
-		std::multimap< uint32_t , uint32_t > my_multimap;  // limit to 32 bits, <Key, Value> = <mask, value>
+		MyMmap my_multimap;  // limit to 32 bits, <Key, Value> = <mask, value>
 		std::unordered_set< uint32_t > my_input;  // for storing every inputs
 };
 
