d727c0e5ad1e4be86abe9301d6e70643ab213fd0
Chocobo1 <Chocobo1@users.noreply.github.com>
2014-03-12 01:02:58 +0800

    replace my naive code of calculating Hamming weight to a high-performance & branch-less version however, it didn't makes much improvement on i7-4770, for 611222653 calls (g++ -O2), mine is  0.01 sec slower, maybe other CPU architecture will have more significant results

--- common.cpp
+++ common.cpp
@@ -13,44 +13,14 @@ bool checkMasked( const uint32_t mask , const uint32_t a , const uint32_t b )
 
 bool diffOneBit( const uint32_t a , const uint32_t b )
 {
-	const uint32_t c = a xor b;
-	switch( c )
-	{
-		case 1<<0:
-		case 1<<1:
-		case 1<<2:
-		case 1<<3:
-		case 1<<4:
-		case 1<<5:
-		case 1<<6:
-		case 1<<7:
-		case 1<<8:
-		case 1<<9:
-		case 1<<10:
-		case 1<<11:
-		case 1<<12:
-		case 1<<13:
-		case 1<<14:
-		case 1<<15:
-		case 1<<16:
-		case 1<<17:
-		case 1<<18:
-		case 1<<19:
-		case 1<<20:
-		case 1<<21:
-		case 1<<22:
-		case 1<<23:
-		case 1<<24:
-		case 1<<25:
-		case 1<<26:
-		case 1<<27:
-		case 1<<28:
-		case 1<<29:
-		case 1<<30:
-		case 1<<31:
-		{
-			return true;
-		}
-	}
-	return false;
+	// Calculating Hamming weight
+	// see http://stackoverflow.com/questions/109023/how-to-count-the-number-of-set-bits-in-a-32-bit-integer
+	// and http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetParallel
+
+	uint32_t i = a xor b;
+
+	i = i - ( ( i >> 1 ) & 0x55555555 );
+	i = ( i & 0x33333333 ) + ( ( i >> 2 ) & 0x33333333 );
+	i = ( ( ( ( i + ( i >> 4 ) ) & 0x0F0F0F0F ) * 0x01010101 ) >> 24 );
+	return ( i == 1 ) ? true : false;
 }
